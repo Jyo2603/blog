0bc99cdc827a832c2ee26f496a7c1e5b78aab0f9
tanu-04 <129984837+tanu-04@users.noreply.github.com>
2025-05-20 18:39:35 +0000

    new_commit3

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import '/workspaces/blog/my-app/src/index.css';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 
 import { Link } from "react-router-dom";
 
